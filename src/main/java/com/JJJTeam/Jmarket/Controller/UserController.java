b63e62446e7b1df7a90c5d159ee26a49f34f6ba4
hodob <hodob76@gmail.com>
2022-09-25 11:37:02 +0900

    pom.xml수정

--- src/main/java/com/JJJTeam/Jmarket/Controller/UserController.java
+++ src/main/java/com/JJJTeam/Jmarket/Controller/UserController.java
@@ -8,21 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
+//	로그인 페이지로 이동
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginPage(Model model) {
 		logger.info("this page is Login page {}.", model);
-
 		model.addAttribute("model", "LoginController model test");
-		
 		return "user/login";
 	}
+	
+//	로그인 페이지에서 로그인 했을때 프로세스 처리
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String userLoginProcess(Model model, String userId, String userPwd) {
 		logger.info("로그인 프로세스 과정 {}.", model);
