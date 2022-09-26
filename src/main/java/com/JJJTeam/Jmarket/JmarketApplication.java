565a9af5784dcd431e148072723839bb6a982b7f
hodob <hodob76@gmail.com>
2022-09-26 11:40:40 +0900

    spring boot

---
+++ src/main/java/com/JJJTeam/Jmarket/JmarketApplication.java
@@ -0,0 +1,13 @@
+package com.JJJTeam.Jmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JJJTeam/Jmarket/JmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JJJTeam.Jmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
