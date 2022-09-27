53fe7353b94ff565f42cfc731abe8f8f34962840
hodob <hodob76@gmail.com>
2022-09-27 22:33:13 +0900

    DTO확인. mybatis설정추가,

--- src/main/java/com/JJJTeam/Jmarket/Controller/UserController.java
+++ src/main/java/com/JJJTeam/Jmarket/Controller/UserController.java
@@ -2,6 +2,9 @@ package com.JJJTeam.Jmarket.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.JJJTeam.Jmarket.DTO.UserDTO;
 
 @Controller
 public class UserController {
@@ -23,6 +26,17 @@ public class UserController {
 		
 		try {
 			
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+		
+		return "/user/signUpPage";
+	}
+	@PostMapping("/user/signUpProcess")
+	public String signUpProcess(UserDTO userDTO) throws Exception {
+		
+		try {
+
 		} catch (Exception e) {
 			throw new Exception(e.getMessage()); 
 		}
---
+++ src/main/java/com/JJJTeam/Jmarket/DTO/UserDTO.java
@@ -0,0 +1,14 @@
+package com.JJJTeam.Jmarket.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDTO {
+	private String id;
+	private String pw;
+
+}
