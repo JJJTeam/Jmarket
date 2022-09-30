7820c817230f6e851c15aa6c566092fc462ce3e0
hodob <hodob76@gmail.com>
2022-09-30 10:16:09 +0900

    패키지 명 수정

--- src/main/java/com/JJJTeam/Jmarket/JmarketApplication.java
+++ src/main/java/com/JJJTeam/Jmarket/JmarketApplication.java
@@ -1,4 +1,4 @@
-package com.JJJTeam.Jmarket;
+package com.jjjteam.jmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/JJJTeam/Jmarket/JmarketApplicationTests.java
+++ src/test/java/com/JJJTeam/Jmarket/JmarketApplicationTests.java
@@ -1,4 +1,4 @@
-package com.JJJTeam.Jmarket;
+package com.jjjteam.jmarket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
