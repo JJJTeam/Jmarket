4aa44eb06321174d0b36e2a0e4789094564b2382
JEONGSUJIN <peekaboo32@naver.com>
2022-09-30 09:49:31 +0900

    수진회원가입로그인만들브랜치생성: Auto stash before rebase of "origin/수진회원가입로그인만들브랜치생성"

--- src/main/java/com/JJJTeam/Jmarket/mapper/UserMapper.java
+++ src/main/java/com/JJJTeam/Jmarket/mapper/UserMapper.java
@@ -2,3 +2,16 @@ package com.JJJTeam.Jmarket.mapper;
 
 import com.JJJTeam.Jmarket.DTO.UserDTO;
 
+public interface UserMapper {
+<<<<<<< HEAD
+	int signUp(UserDTO userDTO);
+=======
+	 @InsertProvider(type = UserSQL.class, method = "UserSingUp")
+
+	 public void UserSingUp(UserDTO userDTO);
+
+	 
+	 
+
+>>>>>>> 71ab30c... 수진브런치로이동
+}
