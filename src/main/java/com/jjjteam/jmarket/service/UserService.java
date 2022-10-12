6bb259c48afd9339aec87e5626839b735703165c
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-12 17:38:14 +0900

    클래스 수정

--- src/main/java/com/jjjteam/jmarket/service/UserService.java
+++ src/main/java/com/jjjteam/jmarket/service/UserService.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jjjteam.jmarket.dto.UserDTO;
-import com.jjjteam.jmarket.mapper.UserMapper;
 
 
 
 @Service
 public class UserService {
-	 @Autowired UserMapper mapper;
-	   public void signUp(UserDTO userDTO) {
-	      mapper.UserSingUp(userDTO);
-	   }
+	
 }
