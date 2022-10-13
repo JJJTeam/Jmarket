b2436515e96c509c605013d3eb157fca2d9026e3
hodob <hodob76@gmail.com>
2022-10-13 12:14:15 +0900

    JPA Bean 등록 에러수정

--- src/main/java/com/jjjteam/jmarket/repository/AccountRepository.java
+++ src/main/java/com/jjjteam/jmarket/repository/AccountRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 //ibatis/MyBatis 등에서 Dao라고 불리는 DB Layer 접근자
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    Optional<Account> findByUserId(String userId);
+    Optional<Account> findById(String userId);
 }
