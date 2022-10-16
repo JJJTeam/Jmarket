a0c41ffdb6f807fd9bf97c9076f21a1bded2c9c5
hodob <hodob76@gmail.com>
2022-10-16 09:13:13 +0900

    Logger Slf4j 추가 설정

--- src/main/java/com/jjjteam/jmarket/controller/AccountController.java
+++ src/main/java/com/jjjteam/jmarket/controller/AccountController.java
@@ -6,6 +6,9 @@ import com.jjjteam.jmarket.dto.AccountFormDTO;
 import com.jjjteam.jmarket.model.Account;
 import com.jjjteam.jmarket.service.AccountServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -17,15 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/account")
+@Slf4j
 public class AccountController {
+    private final Logger logger = LoggerFactory.getLogger("LoggerController 의 로그");
+
 
     private final AccountServiceImpl accountService;
 
     @PostMapping("")
-    public ResponseEntity<?> insertAccount(
-            @Valid @RequestBody AccountFormDTO dto,
-            BindingResult bindingResult
-    ) {
+    public ResponseEntity<?> insertAccount(@Valid @RequestBody AccountFormDTO dto, BindingResult bindingResult) {
+        logger.trace("로깅 발생!{}",dto);
+        log.info("log");
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
