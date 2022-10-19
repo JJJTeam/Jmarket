1cb9998b4081b7c792b72dc519cff8ada230881a
JEONGSUJIN <peekaboo32@naver.com>
2022-10-19 13:56:39 +0900

    수정

--- src/main/java/com/jjjteam/jmarket/controller/BoardController.java
+++ src/main/java/com/jjjteam/jmarket/controller/BoardController.java
@@ -1,13 +1,14 @@
 package com.jjjteam.jmarket.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController{
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String board() {
 		return "list";
 	}
