7102682b5c4b97681be331d2a87e5266850e9e22
JEONGSUJIN <peekaboo32@naver.com>
2022-10-19 13:49:28 +0900

    클래스 추가

--- src/main/java/com/jjjteam/jmarket/controller/BoardController.java
+++ src/main/java/com/jjjteam/jmarket/controller/BoardController.java
@@ -1,4 +1,16 @@
 package com.jjjteam.jmarket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/board")
+public class BoardController{
+	
+	@RequestMapping("/list")
+	public String board() {
+		return "list";
+	}
+	
+} 
 
