e79c4cbe4be8bf3bf60cd07c9ee1472812a0b5c5
hodob <hodob76@gmail.com>
2022-10-20 14:05:51 +0900

    before main merge

---
+++ src/main/java/com/jjjteam/jmarket/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.jjjteam.jmarket.repository;
+
+import com.jjjteam.jmarket.model.ERole;
+import com.jjjteam.jmarket.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
