2b42b30cea84f43eb37caa1339e3980c5ce7e3b4
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-28 16:14:54 +0900

    Board Service 로직수정

--- src/main/java/com/jjjteam/jmarket/service/BoardService.java
+++ src/main/java/com/jjjteam/jmarket/service/BoardService.java
@@ -1,34 +1,9 @@
 package com.jjjteam.jmarket.service;
 
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.jjjteam.jmarket.model.Board;
-import com.jjjteam.jmarket.model.User;
-import com.jjjteam.jmarket.repository.BoardRepository;
 
 @Service
 public class BoardService {
 
-    @Autowired
-    private BoardRepository boardRepository;
-
-    public void write(Board board){
-
-        boardRepository.save(board);
-    }
-    
-    //게시글 리스트 처리
-    public List<Board> boardList(){
-        return  boardRepository.findAll();
-    }
-    
     
 }
