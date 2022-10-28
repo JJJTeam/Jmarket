c7b459b4f53ec2430d155aeb26c193a536639b3f
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-29 01:16:59 +0900

    공지사항 서비스 로직

--- src/main/java/com/jjjteam/jmarket/service/BoardService.java
+++ src/main/java/com/jjjteam/jmarket/service/BoardService.java
@@ -1,9 +1,38 @@
 package com.jjjteam.jmarket.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.jjjteam.jmarket.model.Board;
+import com.jjjteam.jmarket.repository.BoardRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class BoardService {
 
-    
+	//생성자로 주입
+	private final BoardRepository boardRepository;
+	
+	//게시물 등록
+	public void create(String subject, String content) {
+		Board board = new Board();
+		board.setSubject(subject);
+		board.setContent(content);
+		board.setCreateDate(LocalDateTime.now());
+		this.boardRepository.save(board);
+	}
+	
+	//게시물 리스트
+	public List<Board> getList(){
+		return this.boardRepository.findAll();
+	}
+	
+	//질문상세보기 리스트
+	
+	
+	
 }
