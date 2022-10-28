c3cd54392c6ac58988f02f74f06bb864b03e2c30
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-28 16:27:46 +0900

    기존 boarddto 삭제

--- src/main/java/com/jjjteam/jmarket/dto/BoardDTO.java
+++ src/main/java/com/jjjteam/jmarket/dto/BoardDTO.java
@@ -2,29 +2,6 @@ package com.jjjteam.jmarket.dto;
 
 import lombok.Data;
 
-@Data
 public class BoardDTO {
 
-	private int uid;
-	private String subject;
-	private String contents;
-	private String name;
-	private String viewcont;
-	private String regdate;
-	
-	public BoardDTO() {
-		
-	}
-	
-	public BoardDTO(int uid, String subject, String contents, String name, String viewcont , String regdate) {
-		this.uid = uid;
-		this.subject = subject;
-		this.contents = contents;
-		this.name = name;
-		this.viewcont = viewcont;
-		this.regdate = regdate;
-	}
-	
-	
-	
 }
