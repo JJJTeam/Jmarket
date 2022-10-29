8e8d776b7ef36e094818af3e70f26a1b41102b18
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-29 17:53:05 +0900

    게시판글쓰기 맵핑

--- src/main/java/com/jjjteam/jmarket/controller/BoardController.java
+++ src/main/java/com/jjjteam/jmarket/controller/BoardController.java
@@ -11,11 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jjjteam.jmarket.effectiveness.BoardForm;
@@ -24,8 +20,6 @@ import com.jjjteam.jmarket.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
 
-import com.jjjteam.jmarket.model.Board;
-import com.jjjteam.jmarket.service.BoardService;
 
 
 @Controller
@@ -37,19 +31,20 @@ public class BoardController {
 	private final BoardService boardService;
 	
 	//게시물등록페이지 이동
+	
 	@GetMapping("/register_form")
 	public String registerForm(BoardForm boardForm) {
 		return "board/register";
 	}
 	
 	//게시물등록
-	@PostMapping("/register_form")
-	public String registerForm(@Valid BoardForm boardForm, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
-			return "/register_form";
-		}
-		return "redirect:board/list";
-	}
+//	@PostMapping("/register_form")
+//	public String registerForm(@Valid BoardForm boardForm, BindingResult bindingResult) {
+//		if(bindingResult.hasErrors()) {
+//			return "/register_form";
+//		}
+//		return "redirect:board/list";
+//	}
 	
 	
 	    
