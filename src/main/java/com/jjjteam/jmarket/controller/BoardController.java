42b09327b5e245af2d8b6347109f67d0ad3f93bb
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-30 00:35:24 +0900

    뷰 페이지 community 클릭시 게시판으로 이동하는 부분 수정  뷰페이지  수정은 낼 하는걸루

--- src/main/java/com/jjjteam/jmarket/controller/BoardController.java
+++ src/main/java/com/jjjteam/jmarket/controller/BoardController.java
@@ -1,26 +1,17 @@
 package com.jjjteam.jmarket.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.jjjteam.jmarket.effectiveness.BoardForm;
+
 import com.jjjteam.jmarket.model.Board;
 import com.jjjteam.jmarket.repository.BoardRepository;
 import com.jjjteam.jmarket.service.BoardService;
@@ -70,15 +61,7 @@ public class BoardController {
 	}
 	
 	
-	
-		
-		
-	
-	
-	
-	
 
-	
 	
 	    
 }
