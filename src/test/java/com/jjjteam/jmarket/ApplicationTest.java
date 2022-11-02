1c68a6dac56d95491745362b8b31854b9678d6cd
JTB_0816 <nnnn@naver.com>
2022-11-02 16:22:57 +0900

    test파일 수정

--- src/test/java/com/jjjteam/jmarket/ApplicationTest.java
+++ src/test/java/com/jjjteam/jmarket/ApplicationTest.java
@@ -1,28 +1,11 @@
-	@Autowired
-	private BoardRepository boardRepository;
-	@Test
-	void testJpa() {
-		Board b1 = new Board();
-		b1.setSubject("board에 잘들어가는지 체크1111111");
-		b1.setContent("안에 들어가는 content 체크까지 1111");
-		b1.setCreateDate(LocalDateTime.now());
-		this.boardRepository.save(b1);
-		Board b2 = new Board();
-		b2.setSubject("board에 잘들어가는지 체크2222222");
-		b2.setContent("안에 들어가는 content 체크까지22222");
-		b2.setCreateDate(LocalDateTime.now());
-	}
-		this.boardRepository.save(b2);
-}
 package com.jjjteam.jmarket;
 
-
-
-
+import com.jjjteam.jmarket.model.Board;
 import com.jjjteam.jmarket.model.ERole;
 import com.jjjteam.jmarket.model.Role;
 import com.jjjteam.jmarket.model.User;
 import com.jjjteam.jmarket.model.UserAddress;
+import com.jjjteam.jmarket.repository.BoardRepository;
 import com.jjjteam.jmarket.repository.RoleRepository;
 import com.jjjteam.jmarket.repository.UserAddressRepository;
 import com.jjjteam.jmarket.repository.UserRepository;
@@ -33,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -41,7 +25,6 @@ import java.util.Set;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
@@ -49,7 +32,7 @@ public class ApplicationTest {
 	@Autowired
 	private UserAddressRepository userAddressRepository;
 	@Autowired
-    private UserRepository userRepository;
+	private UserRepository userRepository;
 	@Autowired
 	PasswordEncoder encoder;
 	@Autowired
@@ -67,24 +50,30 @@ public class ApplicationTest {
 	@Test
 	public void userInsert() {
 
-
 	}
+
 	@Test
-	public void userAddressInsert(){
+	public void userAddressInsert() {
 
-
-		userAddressRepository.save(
-				UserAddress.builder()
-						.address("test8")
-						.user(userRepository.getReferenceById(1L))
-						.build()
-		);
-
+		userAddressRepository.save(UserAddress.builder().address("test8").user(userRepository.getReferenceById(1L)).build());
 
 	}
 
+	@Autowired
+	private BoardRepository boardRepository;
 
-
-
-
+	@Test
+	void testJpa() {
+		Board b1 = new Board();
+		b1.setSubject("board에 잘들어가는지 체크1111111");
+		b1.setContent("안에 들어가는 content 체크까지 1111");
+		b1.setCreateDate(LocalDateTime.now());
+		this.boardRepository.save(b1);
+		Board b2 = new Board();
+		b2.setSubject("board에 잘들어가는지 체크2222222");
+		b2.setContent("안에 들어가는 content 체크까지22222");
+		b2.setCreateDate(LocalDateTime.now());
+		
+		this.boardRepository.save(b2);
+	}
 }
