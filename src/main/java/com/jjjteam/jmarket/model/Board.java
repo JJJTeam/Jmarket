e74998da9388f6e82d7749647f5b336f23c1ef36
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-04 23:59:51 +0900

    @ManyToOne(fetch = FetchType.EAGER) 	@JoinColumn(name="userId") 추가

--- src/main/java/com/jjjteam/jmarket/model/Board.java
+++ src/main/java/com/jjjteam/jmarket/model/Board.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
@@ -32,7 +34,8 @@ public class Board {
 	
 	
 	//게시판에 글쓴이 추가
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="userId")
 	private User user;
 
 
