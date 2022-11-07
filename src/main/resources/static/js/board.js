5642d05b86634ef1d4533a0e8247585644cbeb25
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-08 08:48:32 +0900

    파일설정부분 커밋

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//onImageUpload ajax 함수
+function uploadSummernoteImageFile(file, editor) {
+            data = new FormData();
+            data.append("file", file);
+            $.ajax({
+                data : data,
+                type : "POST",
+                url : "/uploadSummernoteImageFile",
+                contentType : false,
+                processData : false,
+                success : function(data) {
+                    //항상 업로드된 파일의 url이 있어야 한다.
+                    $(editor).summernote('insertImage', data.url);
+                }
+            });
+        }
+        
