9724bbabff2fee65a49df43bddd70403fd112b0f
hodob <hodob76@gmail.com>
2022-11-21 11:19:50 +0900

    devtool 설정, 경로 수정

--- src/main/java/com/jjjteam/jmarket/controller/UserController.java
+++ src/main/java/com/jjjteam/jmarket/controller/UserController.java
@@ -3,7 +3,6 @@ package com.jjjteam.jmarket.controller;
 
 
 import com.jjjteam.jmarket.dto.UserAddressDTO;
-import com.jjjteam.jmarket.dto.UserDTO;
 import com.jjjteam.jmarket.security.services.UserDetailsImpl;
 import com.jjjteam.jmarket.service.UserAddressService;
 import com.jjjteam.jmarket.service.UserService;
@@ -13,12 +12,8 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.util.List;
 
 
@@ -38,14 +33,14 @@ public class UserController {
 	public String ToMyPageAddressList(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		List<UserAddressDTO> addressList = userAddressService.loadAddressListByUserId(userDetails.getId());
 		model.addAttribute("addressList",addressList);
-		return "/mypage/mypageAddress";
+		return "/mypage/address";
 	}
 
 	@GetMapping("/info")
 	public String ToMyPageUserInfo(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		List<UserAddressDTO> addressList = userAddressService.loadAddressListByUserId(userDetails.getId());
 		model.addAttribute("addressList",addressList);
-		return "/mypage/mypageAddress";
+		return "/mypage/address";
 	}
 
 
