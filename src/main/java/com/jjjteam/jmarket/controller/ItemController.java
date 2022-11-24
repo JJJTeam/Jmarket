ac733756ecb22ae0bd42462ffa7d873cef857ddf
hodob <hodob76@gmail.com>
2022-11-24 13:49:20 +0900

    hotfix

--- src/main/java/com/jjjteam/jmarket/controller/ItemController.java
+++ src/main/java/com/jjjteam/jmarket/controller/ItemController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
