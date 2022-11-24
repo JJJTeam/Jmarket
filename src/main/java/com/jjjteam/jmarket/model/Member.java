9e8b4e3dc250612ae1daf8268ca7e68978d8edc3
hodob <hodob76@gmail.com>
2022-11-24 15:51:41 +0900

    Member주석

--- src/main/java/com/jjjteam/jmarket/model/Member.java
+++ src/main/java/com/jjjteam/jmarket/model/Member.java
@@ -1,51 +1,51 @@
-package com.jjjteam.jmarket.model;
-
-import com.jjjteam.jmarket.constant.Role_notUSE;
-import com.jjjteam.jmarket.dto.MemberFormDTO;
-import lombok.*;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.persistence.*;
-
-@Setter
-@Entity
-@Getter
-@ToString
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name="member")
-public class Member extends BaseEntity{
-
-    @Id
-    @Column(name="member_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    
-    private String name;
-    
-    @Column(unique = true)
-    private String email;
-    
-    private String password;
-    
-    private String address;
-    
-    @Enumerated(EnumType.STRING)
-    private Role_notUSE roleNotUSE;
-    
-    public static Member createMember(MemberFormDTO memberFormDTO, PasswordEncoder passwordEncoder){
-        
-        Member member = Member.builder()
-                .name(memberFormDTO.getName())
-                .email(memberFormDTO.getEmail())
-                .address(memberFormDTO.getAddress())
-                .password( passwordEncoder.encode( memberFormDTO.getPassword() ) ) // BCryptPasswordEncoder Bean 을 파라미터로 넘겨서 비번을 암호화함
-//                .role(Role.USER)
-                .roleNotUSE(Role_notUSE.ADMIN)
-                .build();
-        
-        return member;
-    }
-    // 여기서는 반대로 ! 이렇게 해줌
-}
+//package com.jjjteam.jmarket.model;
+//
+//import com.jjjteam.jmarket.constant.Role_notUSE;
+//import com.jjjteam.jmarket.dto.MemberFormDTO;
+//import lombok.*;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import javax.persistence.*;
+//
+//@Setter
+//@Entity
+//@Getter
+//@ToString
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Table(name="member")
+//public class Member extends BaseEntity{
+//
+//    @Id
+//    @Column(name="member_id")
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String name;
+//
+//    @Column(unique = true)
+//    private String email;
+//
+//    private String password;
+//
+//    private String address;
+//
+//    @Enumerated(EnumType.STRING)
+//    private Role_notUSE roleNotUSE;
+//
+//    public static Member createMember(MemberFormDTO memberFormDTO, PasswordEncoder passwordEncoder){
+//
+//        Member member = Member.builder()
+//                .name(memberFormDTO.getName())
+//                .email(memberFormDTO.getEmail())
+//                .address(memberFormDTO.getAddress())
+//                .password( passwordEncoder.encode( memberFormDTO.getPassword() ) ) // BCryptPasswordEncoder Bean 을 파라미터로 넘겨서 비번을 암호화함
+////                .role(Role.USER)
+//                .roleNotUSE(Role_notUSE.ADMIN)
+//                .build();
+//
+//        return member;
+//    }
+//    // 여기서는 반대로 ! 이렇게 해줌
+//}
