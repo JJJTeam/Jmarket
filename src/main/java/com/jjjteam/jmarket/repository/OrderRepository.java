64bdc7c15740345d94c5be422d2b96d10fec2b09
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-25 16:16:26 +0900

    @Query어노테이션을 이용하여 주문이력을 조회하는 쿼리

--- src/main/java/com/jjjteam/jmarket/repository/OrderRepository.java
+++ src/main/java/com/jjjteam/jmarket/repository/OrderRepository.java
@@ -3,8 +3,11 @@ package com.jjjteam.jmarket.repository;
 import com.jjjteam.jmarket.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+ * @Query어노테이션을 이용하여 주문이력을 조회하는 쿼리
+ * */
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-
+	
 
 }
