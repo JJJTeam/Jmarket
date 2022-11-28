a7c9aa20fd0bd74b565889b622217add1198ac9b
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-28 13:10:56 +0900

    OrderRepository 와 OrderHistDTO 구현

--- src/main/java/com/jjjteam/jmarket/dto/OrderHistDTO.java
+++ src/main/java/com/jjjteam/jmarket/dto/OrderHistDTO.java
@@ -19,13 +19,6 @@ import lombok.Setter;
 @Setter
 public class OrderHistDTO {
 	
-	private Long orderId; //주문아이
-	private String orderDate; //주문 날짜 
-	private OrderStatus orderStatus; //주문 상태 
-	
-	//주문상품리스트
-	private List<OrderItemDTO> orderItemDTOList =new ArrayList<>();
-	
 	
 	//OrderHistDTO 클래스의 생성자로 order 객체를 파라미터로 받아서 멤버 변수 값을 세팅합니다.
 	//주문날짜의 경우 화면에 "yyyy-mm-dd HH:mm" 형태로 전달하기 위해 포맷을 수정
@@ -34,6 +27,15 @@ public class OrderHistDTO {
 		this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 		this.orderStatus= order.getOrderStatus();
 	}
+	
+
+	private Long orderId; //주문아이
+	private String orderDate; //주문 날짜 
+	private OrderStatus orderStatus; //주문 상태 
+    
+	//주문상품리스트
+	private List<OrderItemDTO> orderItemDTOList =new ArrayList<>();
+	
 	
 	//orderItemDTO 객체를 주문 상품 리스트에 추가하는 메서드 
 	public void addOrderItemDTO(OrderItemDTO orderItemDTO) {
