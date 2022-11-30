fe377c71a90cc51cb820e183c20ee0923ab0f01e
SUJINJEONG012 <peekaboo32@naver.com>
2022-11-30 10:56:28 +0900

    Cart 부분 로직구현중

--- src/main/java/com/jjjteam/jmarket/dto/CartItemDTO.java
+++ src/main/java/com/jjjteam/jmarket/dto/CartItemDTO.java
@@ -1,5 +1,20 @@
 package com.jjjteam.jmarket.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CartItemDTO {
 
+	//회원 한명 다 1개의 장바구니를 가지므로  해당회원의장바구니 생성 
+	@NotNull(message= "상품 아이디는 필수 입력 값 입니다.")
+	private Long itemId;
+	
+	@Min(value= 1, message="최소 1개 이상 담아주세요.")
+	private int count;
+	
 }
