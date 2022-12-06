c66e05852634b0389dbf4b77d517503227a59b7a
hodob <hodob76@gmail.com>
2022-12-06 17:05:41 +0900

    컴인

--- src/main/java/com/jjjteam/jmarket/dto/ItemListDTO.java
+++ src/main/java/com/jjjteam/jmarket/dto/ItemListDTO.java
@@ -14,18 +14,15 @@ public class ItemListDTO {
 	
 	private Long id;
 	
-	private String imgUrl;
-	
 	private String itemNm;
 	
 	private Integer price;
 	
 	
 	@QueryProjection // entity 로 원래 바꿨었는데, 이 어노테이션을 쓰면은 dto 로 객체를 변환할 수 있다.
-    public ItemListDTO(Long id, String itemNm, String imgUrl,Integer price){
+    public ItemListDTO(Long id, String itemNm, Integer price){
         this.id = id;
         this.itemNm = itemNm;
-        this.imgUrl = imgUrl;
         this.price = price;
     }
 
